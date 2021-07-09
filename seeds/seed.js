e1024fb1e876934567dc001e6fc40ff930796288
Haley Parish <heparish@ucdavis.edu>
2021-07-09 14:43:55 -0700

    changed Seller to User

--- seeds/seed.js
+++ seeds/seed.js
@@ -9,11 +9,7 @@ const saleData = require('./testsaleData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-<<<<<<< HEAD
   await User.bulkCreate(userData, {
-=======
-  await Seller.bulkCreate(sellerData, {
->>>>>>> d96bacde482145d182ab3478c2868a15258a09c4
     individualHooks: true,
     returning: true,
   });
