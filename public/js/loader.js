75f9270ebfaf19db2182bde686f8407116e2fb65
Haley Parish <heparish@ucdavis.edu>
2021-07-12 18:43:28 -0700

    loader

---
+++ public/js/loader.js
@@ -0,0 +1,15 @@
+
+const preloader = document.querySelector('.preloader');
+
+const fadeEffect = setInterval(() => {
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 0.1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 200);
+
+window.addEventListener('load', fadeEffect);
